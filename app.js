0ca1ebba595def95ca06ee12f9c04bed54eaab49
LatashaWanjira <latasha.wanjira@gmail.com>
2019-09-03 18:57:10 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const app = new Vue({
+  el:'.main',
+  data: {
+    date:'19th Aug 2019',
+    receiptNo:'7301c5e3-ce5b-c288-78c7-08d724cf177',
+    buyer:'Kelvin Njuguna Ndungu',
+    phone:'+25470xxxxx59',
+    paymentMethod: 'Through Mpesa',
+
+    description:'Daily Car Parking for KBH 254R, Moi Avenue.',
+    time:'09:35 PM',
+    total:'KSH 200'
+  }
+});
